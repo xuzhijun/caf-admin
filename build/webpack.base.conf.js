00aecef0a32aacb1998d5633364bd6a4b1ecb006
xuzhijun <pennyxzj@qq.com>
2017-06-22 10:21:09 +0800

    拆分webpack配置准备，暂用npm run build-n 来打包新的生产版本。

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,69 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    'vendor': './src/vendor',
+    'api': './src/api',
+    // 'index': './src/page/index/index', 'resource/indexOld':
+    // './src/page/resource/indexOld',
+    'resource/index': './src/page/resource/index',
+    'menu/index': './src/page/menu/index',
+    'org/index': './src/page/org/index',
+    'role/index': './src/page/role/index'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: [
+      '.js', '.vue', '.json'
+    ],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        // options: vueLoaderConfig
+        options: {
+              loaders: {
+                scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+                sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
+              }
+            }
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      }, {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      }, {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  }
+}
---
+++ build/webpack.prod.conf.js
@@ -0,0 +1,149 @@
+var path = require('path')
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var extractCSS = new ExtractTextPlugin({
+  filename: utils.assetsPath('css/style.css'),
+  allChunks: true
+});
+var extractSASS = new ExtractTextPlugin({
+  filename: utils.assetsPath('css/icon.css'),
+  allChunks: true
+});
+var env = config.build.env
+var webpackConfig = merge(baseWebpackConfig, {
+  module: {
+    rules: [ {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: extractCSS.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader']
+        })
+      }, {
+        test: /\.scss$/,
+        // use: ['style-loader', 'css-loader', 'sass-loader']
+        use: extractSASS.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }]
+    
+  },
+  devtool: config.build.productionSourceMap ? '#source-map' : false,
+  output: {
+    path: config.build.assetsRoot,
+    filename: utils.assetsPath('js/[name].[chunkhash].js'),
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    extractCSS,
+    extractSASS,
+    // extract css into its own file
+    // new ExtractTextPlugin({
+    //   filename: utils.assetsPath('css/[name].[contenthash].css')
+    // }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: true
+    }),
+    // new OptimizeCSSPlugin({
+    //   cssProcessorOptions: {
+    //     safe: true
+    //   }
+    // }),
+    new HtmlWebpackPlugin({
+      title: '资源',
+      filename: 'html/resource/index.html',
+      template: 'src/index.html',
+      inject: true,
+      chunks: ['resource/index', 'api', 'vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      title: '菜单',
+      filename: 'html/menu/index.html',
+      template: 'src/index.html',
+      inject: true,
+      chunks: ['menu/index', 'api', 'vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      title: '机构',
+      filename: 'html/org/index.html',
+      template: 'src/index.html',
+      inject: true,
+      chunks: ['org/index', 'api', 'vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      title: '授权',
+      filename: 'html/role/index.html',
+      template: 'src/index.html',
+      inject: true,
+      chunks: ['role/index', 'api', 'vendor', 'manifest']
+    }),
+    // split vendor js into its own file
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['api', 'vendor'],
+      minChunks: function (module, count) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, '../node_modules')
+          ) === 0
+        )
+      }
+    }),
+    // extract webpack runtime and module manifest to its own file in order to
+    // prevent vendor hash from being updated whenever app bundle is updated
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['api','vendor']
+    }),
+    // new webpack.optimize.ModuleConcatenationPlugin()
+    // copy custom static assets
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: path.resolve(__dirname, '../static'),
+    //     to: config.build.assetsSubDirectory,
+    //     ignore: ['.*']
+    //   }
+    // ])
+  ]
+})
+
+if (config.build.productionGzip) {
+  var CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+  webpackConfig.plugins.push(
+    new CompressionWebpackPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: new RegExp(
+        '\\.(' +
+        config.build.productionGzipExtensions.join('|') +
+        ')$'
+      ),
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  )
+}
+
+if (config.build.bundleAnalyzerReport) {
+  var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+}
+
+module.exports = webpackConfig
